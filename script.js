169d9123a9826cd02477d0bb3b37da60ce463c94
Jennifer <jennifer.ngt@gmail.com>
2023-03-21 20:51:26 -0500

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-function easy(){
-const easyArr = [];
+const numArr = [];
+let score = 0;
 
-for(let i = 0; i<10; i++) {
-    easyArr.push(Math.floor(Math.random()*(10)));
+function easy() {
+  for (let i = 0; i < 11; i++) {
+    numArr.push(Math.floor(Math.random() * 10));
+  }
+
+  console.log(numArr);
+  document.getElementById("number").innerHTML = numArr[0];
 }
+function med() {
+  for (let i = 0; i < 26; i++) {
+    numArr.push(Math.floor(Math.random() * 10));
+  }
 
-console.log(easyArr);
+  console.log(numArr);
+  document.getElementById("number").innerHTML = numArr[0];
+}
+function hard() {
+  for (let i = 0; i < 51; i++) {
+    numArr.push(Math.floor(Math.random() * 10));
+  }
 
+  console.log(numArr);
+  document.getElementById("number").innerHTML = numArr[0];
+}
+function high() {
+  if (numArr[1] >= numArr[0] === true) {
+    numArr.shift();
+    document.getElementById("number").innerHTML = numArr[0];
+    score += 1;
+    document.getElementById("score").innerHTML = "Score:" + score;
+  }
+}
+function low() {
+  if (numArr[1] <= numArr[0] === true) {
+    numArr.shift();
+    document.getElementById("number").innerHTML = numArr[0];
+    score += 1;
+    document.getElementById("score").innerHTML = "Score:" + score;
+  }
 }
