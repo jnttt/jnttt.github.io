df636fd9c1407d292810f4a625c43da441d3c0d1
Jennifer <jennifer.ngt@gmail.com>
2023-03-23 20:54:49 -0500

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const numArr = [];
 let score = 0;
+const difficulty = document.getElementById("difficulty");
+const hide = document.getElementById("hide");
+let h1 = document.getElementById("h1");
 
 function easy() {
   for (let i = 0; i < 11; i++) {
     numArr.push(Math.floor(Math.random() * 10));
   }
-
+  hide.style.display = "none";
+  difficulty.style.display = "none";
   console.log(numArr);
   document.getElementById("number").innerHTML = numArr[0];
 }
@@ -13,7 +17,8 @@ function med() {
   for (let i = 0; i < 26; i++) {
     numArr.push(Math.floor(Math.random() * 10));
   }
-
+  hide.style.display = "none";
+  difficulty.style.display = "none";
   console.log(numArr);
   document.getElementById("number").innerHTML = numArr[0];
 }
@@ -21,7 +26,8 @@ function hard() {
   for (let i = 0; i < 51; i++) {
     numArr.push(Math.floor(Math.random() * 10));
   }
-
+  hide.style.display = "none";
+  difficulty.style.display = "none";
   console.log(numArr);
   document.getElementById("number").innerHTML = numArr[0];
 }
@@ -30,7 +36,14 @@ function high() {
     numArr.shift();
     document.getElementById("number").innerHTML = numArr[0];
     score += 1;
-    document.getElementById("score").innerHTML = "Score:" + score;
+    document.getElementById("score").innerHTML = "Score: " + score;
+    if (numArr.length === 1) {
+      alert("You win!!!");
+      document.getElementById("h1").innerHTML = "YOU WIN!";
+    }
+  } else if (numArr[1] >= numArr[0] === false && numArr.length > 1) {
+    alert("You lose...");
+    window.location.reload();
   }
 }
 function low() {
@@ -38,6 +51,16 @@ function low() {
     numArr.shift();
     document.getElementById("number").innerHTML = numArr[0];
     score += 1;
-    document.getElementById("score").innerHTML = "Score:" + score;
+    document.getElementById("score").innerHTML = "Score: " + score;
+    if (numArr.length === 1) {
+      alert("You win!!!");
+      document.getElementById("h1").innerHTML = "YOU WIN!";
+    }
+  } else if (numArr[1] <= numArr[0] === false && numArr.length > 1) {
+    alert("You lose...");
+    window.location.reload();
   }
+}
+function restart() {
+  window.location.reload();
 }
